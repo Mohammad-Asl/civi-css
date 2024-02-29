05ce2040cfe7118a1a4f4e6a9226826d25a96f8b
Mohammad Soleymani <mohammad.programmer2003@gmail.com>
2024-03-01 00:37:23 +0330

    compelited header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,44 @@
+import { AddSquare } from 'iconsax-react';
+// import { SearchNormal } from 'iconsax-react';
+import { RiNotionFill } from "react-icons/ri";
+import { FaGithub } from "react-icons/fa";
+
 import './Header.css'
 const Header = () => {
   return (
     <header className="header">
-        <div className="head-right">
-          <h1 className="header-title">Got Talent?</h1>
-          <h1>Meet Opportunity</h1>
-          <p>Company reviews, Salaries, Interviews, Jobs</p>
-          <div>
-            <input></input>
-          </div>
+      <div className='header-left-first'>
+        <h1 className="header-title">Got Talent ?</h1>
+        <h1 className='header-title-2'>Meet Opportunity</h1>
+        <p className='header-title-3'>Company reviews, Salaries, Interviews, Jobs</p>
+      </div>
+
+      <div className='header-search'>
+        <input className='header-input' placeholder='Search Jobs'></input>
+      </div>
+
+      <div className='header-popular'>
+        <p className='header-popular-1'>Popular Searches:</p>
+        <p className='header-popular-2'>Data Management , Marketing Manager , Customer Suport</p>
+      </div>
+      <p className='header-brand-startups'>Trusted by leading brands and startups</p>
+      <div className='header-pages'>
+        <div className='header-pages-1'>
+          <AddSquare size="20" />
+          <span className='header-square'>Square</span>
         </div>
+        <div className='header-pages-2'>
+          <RiNotionFill />
+          <span className='header-notion'>Notion</span>
+        </div>
+        <div>
+          <div className='header-pages-3'>
+            <FaGithub />
+            <span className='header-pages-3'>GitHub</span>
+          </div>
+
+        </div>
+      </div>
     </header>
   )
 }
